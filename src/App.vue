c20ad9f3b0e7e76e37c285258e28b4290c2265da
youngjeJO <yj01591@gmail.com>
2022-07-29 09:46:49 +0900

    learn v-for

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div></div>
+  <ul>
+    <li v-for="item in products" :key="item">
+      <!-- '(item,index) in products'로 index를 추출 할 수 있음 index를 key에도 활용 가능 -->
+      {{ item }}
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
   components: {},
 };
 </script>
 
-<style>
-n-top: 60px;
-}
-</style>
+<style></style>
