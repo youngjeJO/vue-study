8a47d6deb03132683275249aef1d76050f56f340
youngjeJO <yj01591@gmail.com>
2022-07-29 11:20:51 +0900

    learn event handler

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <ul>
-    <li v-for="item in products" :key="item">
+    <li v-for="(item, index) in products" :key="index">
       <!-- '(item,index) in products'로 index를 추출 할 수 있음 index를 key에도 활용 가능 -->
       {{ item }}
+      <button @click="increase(index)">신고하기</button>
+      <span>신고수 : {{ 신고[index] }}</span>
     </li>
   </ul>
 </template>
@@ -12,8 +14,18 @@ export default {
   name: "App",
   data() {
     return {
-      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸", "동작구원룸"],
+      신고: new Array(this.products),
     };
+  },
+  methods: {
+    increase(index) {
+      if (!this.신고[index]) {
+        this.신고[index] = 1;
+      } else {
+        this.신고[index] += 1;
+      }
+    },
   },
   components: {},
 };
